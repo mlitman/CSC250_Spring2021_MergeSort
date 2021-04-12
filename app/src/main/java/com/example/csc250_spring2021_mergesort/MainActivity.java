0b447d262cdc41befebdf72743a3acecd855db37
mlitman <awesomefat@gmail.com>
2021-04-12 11:48:19 -0500

    solution for Factorial recursive example

--- app/src/main/java/com/example/csc250_spring2021_mergesort/MainActivity.java
+++ app/src/main/java/com/example/csc250_spring2021_mergesort/MainActivity.java
@@ -21,10 +21,34 @@ public class MainActivity extends AppCompatActivity
         this.answerTV = this.findViewById(R.id.answerTV);
     }
 
+    private int factorialIter(int n)
+    {
+        int runningTotal = n;
+        for(int i = n - 1; i > 0; i--)
+        {
+            runningTotal *= i;
+        }
+        return runningTotal;
+    }
+
+    private int factorialRec(int n)
+    {
+        if(n == 1) return 1;
+        return n * factorialRec(n-1);
+    }
+
+    private int factorialRec2(int n)
+    {
+        //Inline if statement
+        // boolean_exp?true_exp;false_exp;
+        return n == 1?1:n * factorialRec2(n-1);
+    }
+
     public void onFactorialButtonClicked(View v)
     {
         String currValue = this.inputET.getText().toString();
-        this.answerTV.setText(currValue);
+        int currValueAsInt = Integer.parseInt(currValue);
+        this.answerTV.setText("" + this.factorialRec2(currValueAsInt));
     }
 
 
